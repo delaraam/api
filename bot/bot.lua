38b4910693a5ef69a0f2ca3859501cc79562157b
mehdi poinshtan <33298289+poinshtan@users.noreply.github.com>
2018-01-18 15:47:41 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,8 +1,8 @@
 package.path = package.path..';.luarocks/share/lua/5.2/?.lua;.luarocks/share/lua/5.2/?/init.lua'
 package.cpath = package.cpath..';.luarocks/lib/lua/5.2/?.so'
-bot_token = "token" --Put You Token Here
+bot_token = "توکن" --Put You Token Here
 send_api = "https://api.telegram.org/bot"..bot_token
-sudo_id = 111111111
+sudo_id = ایدی عددی مدیر
 require('./bot/methods')
 require('./bot/utils')
 require('./libs/JSON')
@@ -162,7 +162,7 @@ function create_config( )
     "core",
     "plugins"
     },
-    sudo_users = {owner id,cli id},--Sudo users
+    sudo_users = {owner,cli},--Sudo users
     admins = {},
     disabled_channels = {},
     moderation = {data = ''..poinshtan..'/data/moderation.json'},
